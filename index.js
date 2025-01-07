f34f273d1f072eadf02ae0b76e868011811ab304
555Viktor <victor.vaskov2002@gmail.com>
2025-01-07 16:35:19 +0200

    Add board creation module

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const playerModule = (function () {
         createPlayer
     };
 })();
+
+const gameBoardModule = (function () {
+    const cellsArr = [];
+
+    function createBoardCells() {
+        const CELL_COUNT = 9;
+        const gameContainer = document.querySelector('.game-container');
+
+        for (let i = 0; i < CELL_COUNT; i++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            cellsArr.push(cell);
+            gameContainer.appendChild(cell);
+        }
+    }
+
+    return {
+        createBoardCells,
+    }
+})();
+
+gameBoardModule.createBoardCells();
